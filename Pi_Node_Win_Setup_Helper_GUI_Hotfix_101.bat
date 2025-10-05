1072dde9c175c2a16b1397839ab15157f117150c
Fingerkrampf <fingerphone@live.com>
2025-10-05 14:28:39 +0200

    Delete Pi_Node_Win_Setup_Helper_GUI_Hotfix_101.bat
    no needed anymore

--- Pi_Node_Win_Setup_Helper_GUI_Hotfix_101.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-net session >nul 2>&1 || (echo Bitte als Administrator ausführen! & pause & exit)
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Pi Network" /t REG_SZ /d "" /f
-echo Pi Network Autostart-Eintrag erfolgreich deaktiviert!
-timeout /t 2 /nobreak >nul
